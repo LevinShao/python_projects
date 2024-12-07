952cc88e7145ec6f5b558cfe88990f390976daab
Pixel Lemons <levin.shao@education.nsw.gov.au>
2024-12-07 14:37:25 +1100

    minor update

--- first_program/main.py
+++ first_program/main.py
@@ -1,19 +1,4 @@
-import requests
+word1 = input("First word? ")
+word2 = input("Second word? ")
 
-print("No, I will never get off Reddit!")
-
-name = "Levin"
-print(f"Hello, {name}!")
-
-def fetch_joke():
-    response = requests.get('https://icanhazdadjoke.com/', headers = {'Accept': 'application/json'})
-    joke = response.json()['joke']
-    return joke
-
-def main():
-    joke = fetch_joke()
-    print(f"Here's a random joke for you:")
-    print(joke)
-
-if __name__ == '__main__':
-    main()
+print(f"{word1} and {word2}")
